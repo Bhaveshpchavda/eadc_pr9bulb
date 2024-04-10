93db2ff71c8b809b33f069b0a577ce080e7cc887
Bhavesh Chavda <108624512+Bhaveshpchavda@users.noreply.github.com>
2024-04-10 08:41:42 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -14,3 +14,14 @@ function toggleBulb() {
   const bulbImg = document.getElementById('bulb-img');
   bulbImg.src = isBulbOn ? 'on_bulb.png' : 'off_bulb.png';
 }
+window.watsonAssistantChatOptions = {
+  integrationID: "4d3f4cb6-1b7f-4273-97bb-c4cd616b278c", // The ID of this integration.
+  region: "eu-gb", // The region your integration is hosted in.
+  serviceInstanceID: "bd74a283-2299-4090-8081-9fa96b9fd719", // The ID of your service instance.
+  onLoad: async (instance) => { await instance.render(); }
+};
+setTimeout(function(){
+  const t=document.createElement('script');
+  t.src="https://web-chat.global.assistant.watson.appdomain.cloud/versions/" + (window.watsonAssistantChatOptions.clientVersion || 'latest') + "/WatsonAssistantChatEntry.js";
+  document.head.appendChild(t);
+});
