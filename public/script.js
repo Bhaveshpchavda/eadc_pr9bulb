3dc029e50952c3047423338bb81bb3334ea036bd
Bhavesh Chavda <108624512+Bhaveshpchavda@users.noreply.github.com>
2024-04-15 07:46:05 +0530

    chatbot script.js

--- public/script.js
+++ public/script.js
@@ -15,7 +15,7 @@ function toggleBulb() {
   bulbImg.src = isBulbOn ? 'on_bulb.png' : 'off_bulb.png';
   
 window.watsonAssistantChatOptions = {
-  integrationID: "447d694e-d915-4804-a0b7-442dafed7e27", // The ID of this integration.
+  integrationID: "4d3f4cb6-1b7f-4273-97bb-c4cd616b278c", // The ID of this integration.
   region: "eu-gb", // The region your integration is hosted in.
   serviceInstanceID: "bd74a283-2299-4090-8081-9fa96b9fd719", // The ID of your service instance.
   onLoad: async (instance) => { await instance.render(); }
@@ -25,4 +25,5 @@ setTimeout(function(){
   t.src="https://web-chat.global.assistant.watson.appdomain.cloud/versions/" + (window.watsonAssistantChatOptions.clientVersion || 'latest') + "/WatsonAssistantChatEntry.js";
   document.head.appendChild(t);
 });
+
 
